8d578c4c614f9916576be5d12238f4e3bce13b70
David Adarme <timdavidarm@pm.me>
2023-08-14 18:32:48 -0500

    v2 version

--- db/Script_DDL.sql
+++ db/Script_DDL.sql
@@ -1,193 +1,240 @@
-CREATE TABLE tab_usuario(
-  consec_usu BIGINT DEFAULT NEXTVAL('consec_usu'),
-  id_usu iNTEGER NOT NULL,
-  fec_reg TIMESTAMP WITHOUT TIME ZONE NOT NULL,
-  nom_usu VARCHAR NOT NULL,
-  ape_usu VARCHAR NOT NULL,
-  tel_usu VARCHAR NOT NULL,
-  email_usu VARCHAR NOT NULL,
+CREATE TABLE tabAdministrador(
+  idAdmin INTEGER NOT NULL,
+  fecReg TIMESTAMP WITHOUT TIME ZONE NOT NULL,
+  nomAdmin VARCHAR NOT NULL,
+  apeAdmin VARCHAR NOT NULL,
+  telAdmin VARCHAR NOT NULL,
+  emailAdmin VARCHAR NOT NULL,
   usuario VARCHAR NOT NULL,
   password VARCHAR NOT NULL,
-  estado BOOLEAN NOT NULL DEFAULT true,
-  estado_texto TEXT DEFAULT estado_texto(true),
-  --se creo una funcion "estado_texto" para que true(activo) y false (inactivo)
-  fec_insert TIMESTAMP WITHOUT TIME ZONE,
-  user_insert VARCHAR,
-  fec_update TIMESTAMP WITHOUT TIME ZONe,
-  user_update VARCHAR,
-  PRIMARY KEY (consec_usu)
+  estado TEXT NOT NULL DEFAULT 'ACTIVO',
+  fecInsert TIMESTAMP WITHOUT TIME ZONE,
+  userInsert VARCHAR,
+  fecUpdate TIMESTAMP WITHOUT TIME ZONE,
+  userUpdate VARCHAR,
+  PRIMARY KEY (idAdmin)
 );
 
-CREATE TABLE tab_cliente(
-  consec_cli BIGINT NOT NULL DEFAULT NEXTVAL('consec_cli'),
-  fec_reg TIMESTAMP WITHOUT TIME ZONE NOT NULL,
-  tipo_cli VARCHAR NOT NULL,
-  id_cli INTEGER NOT NULL,
-  nit_cli VARCHAR NOT NULL,
-  nom_cli VARCHAR NOT NULL,
-  ape_cli VARCHAR NOT NULL,
-  nom_empr VARCHAR NOT NULL,
-  tel_cli VARCHAR NOT NULL,
-  email_cli VARCHAR NOT NULL,
-  dir_cli VARCHAR NOT NULL,
-  fec_insert TIMESTAMP WITHOUT TIME ZONE,
-  user_insert VARCHAR,
-  fec_update TIMESTAMP WITHOUT TIME ZONe,
-  user_update VARCHAR,
-  PRIMARY KEY (consec_cli)
+
+CREATE TABLE tabCliente(
+  consecCli BIGINT NOT NULL DEFAULT NEXTVAL('consecCli'),
+  fecReg TIMESTAMP WITHOUT TIME ZONE NOT NULL,
+  tipoCli VARCHAR NOT NULL,
+  PRIMARY KEY (consecCli)
 );
 
-CREATE TABLE tab_proveedor(
-  consec_prov BIGINT NOT NULL DEFAULT NEXTVAL('consec_prov'),
-  nit_prov VARCHAR NOT NULL,
-  fec_reg TIMESTAMP WITHOUT TIME ZONE NOT NULL,
-  nom_prov VARCHAR NOT NULL,
-  tel_prov VARCHAR NOT NULL,
-  email_prov VARCHAR NOT NULL,
-  dir_prov VARCHAR NOT NULL,
-  estado BOOLEAN NOT NULL DEFAULT true,
-  estado_texto TEXT DEFAULT estado_texto(true),
-  fec_insert TIMESTAMP WITHOUT TIME ZONE,
-  user_insert VARCHAR,
-  fec_update TIMESTAMP WITHOUT TIME ZONe,
-  user_update VARCHAR,
-  PRIMARY KEY (consec_prov)
+CREATE TABLE tabClienteNatural(
+  idCli VARCHAR NOT NULL,
+  consecCli BIGINT NOT NULL,
+  fecReg TIMESTAMP WITHOUT TIME ZONE NOT NULL,
+  nomCli VARCHAR ,
+  apeCli VARCHAR ,
+  telCli VARCHAR NOT NULL,
+  emailCli VARCHAR NOT NULL,
+  dirCli VARCHAR NOT NULL,
+  fecInsert TIMESTAMP WITHOUT TIME ZONE,
+  userInsert VARCHAR,
+  fecUpdate TIMESTAMP WITHOUT TIME ZONe,
+  userUpdate VARCHAR,
+  PRIMARY KEY (idCli),
+  CONSTRAINT fkCliNatural
+  FOREIGN KEY (consecCli) REFERENCES tabCliente(consecCli)
 );
 
-CREATE TABLE tab_categoria(
-  consec_categ BIGINT NOT NULL DEFAULT NEXTVAL('consec_categ'),
-  nom_categ VARCHAR NOT NULL,
-  estado BOOLEAN NOT NULL DEFAULT true,
-  estado_texto TEXT DEFAULT estado_texto(true),
-  fec_insert TIMESTAMP WITHOUT TIME ZONE,
-  user_insert VARCHAR,
-  fec_update TIMESTAMP WITHOUT TIME ZONe,
-  user_update VARCHAR,
-  PRIMARY KEY (consec_categ)
+CREATE TABLE tabClienteJuridico(
+  nitCli VARCHAR NOT NULL,
+  consecCli BIGINT NOT NULL,
+  fecReg TIMESTAMP WITHOUT TIME ZONE NOT NULL,
+  nomEmpr VARCHAR ,
+  telEmpr VARCHAR NOT NULL,
+  emailEmpr VARCHAR NOT NULL,
+  dirEmpr VARCHAR NOT NULL,
+  fecInsert TIMESTAMP WITHOUT TIME ZONE,
+  userInsert VARCHAR,
+  fecUpdate TIMESTAMP WITHOUT TIME ZONe,
+  userUpdate VARCHAR,
+  PRIMARY KEY (nitCli),
+  CONSTRAINT fkCliJuridico
+  FOREIGN KEY (consecCli) REFERENCES tabCliente(consecCli)
 );
 
-CREATE TABLE tab_marca(
-  consec_marca BIGINT NOT NULL DEFAULT NEXTVAL('consec_marca'),
-  nom_marca VARCHAR NOT NULL,
-  estado BOOLEAN NOT NULL DEFAULT true,
-  estado_texto TEXT DEFAULT estado_texto(true),
-  fec_insert TIMESTAMP WITHOUT TIME ZONE,
-  user_insert VARCHAR,
-  fec_update TIMESTAMP WITHOUT TIME ZONe,
-  user_update VARCHAR,
-  PRIMARY KEY (consec_marca)
+CREATE TABLE tabProveedor(
+  nitProv VARCHAR NOT NULL,
+  fecReg TIMESTAMP WITHOUT TIME ZONE NOT NULL,
+  nomProv VARCHAR NOT NULL,
+  telProv VARCHAR NOT NULL,
+  emailProv VARCHAR NOT NULL,
+  dirProv VARCHAR NOT NULL,
+  estado TEXT NOT NULL DEFAULT 'ACTIVO',
+  fecInsert TIMESTAMP WITHOUT TIME ZONE,
+  userInsert VARCHAR,
+  fecUpdate TIMESTAMP WITHOUT TIME ZONe,
+  userUpdate VARCHAR,
+  PRIMARY KEY (nitProv)
 );
 
-CREATE TABLE tab_articulo(
-  consec_art BIGINT NOT NULL DEFAULT NEXTVAL('consec_art'),
-  ean_art VARCHAR NOT NULL,
-  fec_reg TIMESTAMP WITHOUT TIME ZONE NOT NULL,
-  nom_art VARCHAR NOT NULL,
-  marca_art VARCHAR NOT NULL,
-  categ_art VARCHAR NOT NULL,
-  descrip_art TEXT,
-  unid_med VARCHAR NOT NULL,
-  val_unit INTEGER NOT NULL,
-  iva INTEGER NOT NULL,
-  val_stock INTEGER NOT NULL,
-  stock_min INTEGER NOT NULL,
-  stock_max INTEGER NOT NULL,
-  val_reorden INTEGER NOT NULL,
-  fec_vence DATE,
-  estado BOOLEAN NOT NULL DEFAULT true,
-  estado_texto TEXT DEFAULT estado_texto(true),
-  fec_insert TIMESTAMP WITHOUT TIME ZONE,
-  user_insert VARCHAR,
-  fec_update TIMESTAMP WITHOUT TIME ZONe,
-  user_update VARCHAR,
-  consec_marca BIGINT NOT NULL,
-  consec_categ BIGINT NOT NULL,
-  PRIMARY KEY (ean_art),
-  FOREIGN KEY (consec_marca) REFERENCES tab_marca(consec_marca),
-  FOREIGN KEY (consec_categ) REFERENCES tab_categoria(consec_categ)
+
+CREATE TABLE tabCategoria(
+  consecCateg BIGINT NOT NULL DEFAULT NEXTVAL('consecCateg'),
+  nomCateg VARCHAR NOT NULL,
+  estado TEXT NOT NULL DEFAULT 'ACTIVO',
+  idAdmin INTEGER NOT NULL,
+  fecInsert TIMESTAMP WITHOUT TIME ZONE,
+  userInsert VARCHAR,
+  fecUpdate TIMESTAMP WITHOUT TIME ZONe,
+  userUpdate VARCHAR,
+  PRIMARY KEY (consecCateg),
+  CONSTRAINT fkAdministrador
+  FOREIGN KEY (idAdmin ) REFERENCES tabAdministrador(idAdmin)
 );
 
-CREATE TABLE tab_artxprov(
-  consec_artxprov BIGINT NOT NULL DEFAULT NEXTVAL('consec_artxprov'),
-  nom_prov VARCHAR NOT NULL,
-  nom_art VARCHAR NOT NULL,
-  val_compra NUMERIC(10, 2) NOT NULL,
-  val_stock INTEGER NOT NULL,
-  fec_insert TIMESTAMP WITHOUT TIME ZONE,
-  user_insert VARCHAR,
-  fec_update TIMESTAMP WITHOUT TIME ZONe,
-  user_update VARCHAR,
-  consec_prov BIGINT NOT NULL,
-  ean_art VARCHAR NOT NULL,
-  PRIMARY KEY (consec_artxprov),
-  FOREIGN KEY (consec_prov) REFERENCES tab_proveedor(consec_prov),
-  FOREIGN KEY (ean_art) REFERENCES tab_articulo(ean_art)
+
+CREATE TABLE tabMarca(
+  consecMarca BIGINT NOT NULL DEFAULT NEXTVAL('consecMarca'),
+  nomMarca VARCHAR NOT NULL,
+  estado TEXT NOT NULL DEFAULT 'ACTIVO',
+  fecInsert TIMESTAMP WITHOUT TIME ZONE,
+  userInsert VARCHAR,
+  fecUpdate TIMESTAMP WITHOUT TIME ZONe,
+  userUpdate VARCHAR,
+  PRIMARY KEY (consecMarca)
 );
 
-CREATE TABLE tab_kardex(
-  consec_kardex BIGINT NOT NULL DEFAULT NEXTVAL('consec_kardex'),
-  fec_mov TIMESTAMP WITHOUT TIME ZONE NOT NULL,
-  tipo_mov VARCHAR NOT NULL,
-  nom_art VARCHAR NOT NULL,
-  cant_art INTEGER NOT NULL,
-  val_compra NUMERIC(10, 2) NOT NULL,
-  val_total NUMERIC(10, 2) NOT NULL,
-  val_prom NUMERIC(10, 2) NOT NULL,
+
+CREATE TABLE tabProveedorMarca(
+  consecProvMarca BIGINT NOT NULL DEFAULT NEXTVAL('consecProvMarca'),
+  nitProv VARCHAR NOT NULL,
+  consecMarca BIGINT NOT NULL,
+  fecInsert TIMESTAMP WITHOUT TIME ZONE,
+  userInsert VARCHAR,
+  fecUpdate TIMESTAMP WITHOUT TIME ZONe,
+  userUpdate VARCHAR,
+  PRIMARY KEY (consecProvMarca),
+  CONSTRAINT fkProveedor
+  FOREIGN KEY (nitProv) REFERENCES tabProveedor(nitProv),
+  CONSTRAINT fkMarca
+  FOREIGN KEY (consecMarca) REFERENCES tabMarca(consecMarca)
+);
+
+
+CREATE TABLE tabArticulo(
+  eanArt VARCHAR NOT NULL,
+  fecReg TIMESTAMP WITHOUT TIME ZONE NOT NULL,
+  nomArt VARCHAR NOT NULL,
+  consecMarca BIGINT NOT NULL,
+  consecCateg BIGINT NOT NULL,
+  descripArt TEXT,
+  valUnit NUMERIC(10),
+  valStock INTEGER,
+  stockMin INTEGER NOT NULL DEFAULT 10,
+  stockMax INTEGER NOT NULL DEFAULT 500,
+  valReorden INTEGER NOT NULL DEFAULT 50,
+  fecVence DATE,
+  estado TEXT NOT NULL DEFAULT 'ACTIVO',
+  fecInsert TIMESTAMP WITHOUT TIME ZONE,
+  userInsert VARCHAR,
+  fecUpdate TIMESTAMP WITHOUT TIME ZONe,
+  userUpdate VARCHAR,
+  PRIMARY KEY (eanArt),
+  CONSTRAINT fkMarca
+  FOREIGN KEY (consecMarca) REFERENCES tabMarca(consecMarca),
+  CONSTRAINT fkCategoria
+  FOREIGN KEY (consecCateg) REFERENCES tabCategoria(consecCateg)
+);
+
+
+CREATE TABLE tabKardex(
+  consecKardex BIGINT NOT NULL DEFAULT NEXTVAL('consecKardex'),
+  fecMov TIMESTAMP WITHOUT TIME ZONE NOT NULL,
+  tipoMov VARCHAR NOT NULL,
+  eanArt VARCHAR NOT NULL,
+  nomArt VARCHAR NOT NULL,
+  cantArt INTEGER NOT NULL,
+  valCompra NUMERIC(10) NOT NULL,
+  valTotal NUMERIC(10) NOT NULL,
+  valProm NUMERIC(10) NOT NULL,
   observacion TEXT,
-  nom_prov VARCHAR NOT NULL,
-  fec_insert TIMESTAMP WITHOUT TIME ZONE,
-  user_insert VARCHAR,
-  fec_update TIMESTAMP WITHOUT TIME ZONe,
-  user_update VARCHAR,
-  ean_art VARCHAR NOT NULL,
-  consec_prov BIGINT NOT NULL,
-  PRIMARY KEY (consec_kardex),
-  FOREIGN KEY (ean_art) REFERENCES tab_articulo(ean_art),
-  FOREIGN KEY (consec_prov) REFERENCES tab_proveedor(consec_prov)
+  nitProv VARCHAR NOT NULL,
+  idAdmin INTEGER NOT NULL,
+  fecInsert TIMESTAMP WITHOUT TIME ZONE,
+  userInsert VARCHAR,
+  fecUpdate TIMESTAMP WITHOUT TIME ZONE,
+  userUpdate VARCHAR,
+  PRIMARY KEY (consecKardex),
+  CONSTRAINT fkArticulo
+  FOREIGN KEY (eanArt) REFERENCES tabArticulo(eanArt),
+  CONSTRAINT fkProveedor
+  FOREIGN KEY (nitProv) REFERENCES tabProveedor(nitProv),
+  CONSTRAINT fkAdministrador
+  FOREIGN KEY (idAdmin) REFERENCES tabAdministrador(idAdmin)
 );
 
-CREATE TABLE tab_encabezado_venta(
-  consec_venta BIGINT NOT NULL DEFAULT NEXTVAL('consec_venta'),
-  fec_venta TIMESTAMP WITHOUT TIME ZONE NOT NULL,
-  id_cli INTEGER NOT NULL,
-  val_pagar NUMERIC(10, 2) NOT NULL,
-  estado BOOLEAN NOT NULL DEFAULT true,
-  estado_texto TEXT DEFAULT estado_texto(true),
-  fec_insert TIMESTAMP WITHOUT TIME ZONE,
-  user_insert VARCHAR,
-  fec_update TIMESTAMP WITHOUT TIME ZONe,
-  user_update VARCHAR,
-  consec_cli BIGINT NOT NULL,
-  PRIMARY KEY (consec_venta),
-  FOREIGN KEY (consec_cli) REFERENCES tab_cliente(consec_cli)
+
+CREATE TABLE tabProveedorArticulo(
+  consecProvArt BIGINT NOT NULL DEFAULT NEXTVAL('consecProvArt'),
+  nitProv VARCHAR NOT NULL,
+  eanArt VARCHAR NOT NULL,
+  fecInsert TIMESTAMP WITHOUT TIME ZONE,
+  userInsert VARCHAR,
+  fecUpdate TIMESTAMP WITHOUT TIME ZONe,
+  userUpdate VARCHAR,
+  PRIMARY KEY (consecProvArt),
+  CONSTRAINT fkProveedor
+  FOREIGN KEY (nitProv) REFERENCES tabProveedor(nitProv),
+  CONSTRAINT fkArticulo
+  FOREIGN KEY (eanArt) REFERENCES tabArticulo(eanArt)
+
 );
 
-CREATE TABLE tab_detalle_venta(
-  consec_det_venta BIGINT NOT NULL DEFAULT NEXTVAL('consec_det_venta'),
-  nom_art VARCHAR NOT NULL,
-  cant_art INTEGER NOT NULL,
-  val_unit INTEGER NOT NULL,
-  Subtotal NUMERIC(10, 2) NOT NULL,
-  Descuento NUMERIC(10, 2) NOT NULL,
-  val_iva NUMERIC(10, 2) NOT NULL,
-  Total_Pagar NUMERIC(10, 2) NOT NULL,
-  estado BOOLEAN NOT NULL DEFAULT true,
-  estado_texto TEXT DEFAULT estado_texto(true),
-  fec_insert TIMESTAMP WITHOUT TIME ZONE,
-  user_insert VARCHAR,
-  fec_update TIMESTAMP WITHOUT TIME ZONe,
-  user_update VARCHAR,
-  consec_venta BIGINT NOT NULL,
-  ean_art VARCHAR NOT NULL,
-  PRIMARY KEY (consec_det_venta),
-  FOREIGN KEY (consec_venta) REFERENCES tab_encabezado_venta(consec_venta),
-  FOREIGN KEY (ean_art) REFERENCES tab_articulo(ean_art)
+
+CREATE TABLE tabEncabezadoVenta(
+  consecVenta BIGINT NOT NULL DEFAULT NEXTVAL('consecVenta'),
+  fecVenta TIMESTAMP WITHOUT TIME ZONE NOT NULL,
+  consecCli BIGINT NOT NULL,
+  totalPagar NUMERIC (10),
+  idAdmin INTEGER NOT NULL,
+  estado TEXT NOT NULL DEFAULT 'ACTIVO',
+  fecInsert TIMESTAMP WITHOUT TIME ZONE,
+  userInsert VARCHAR,
+  fecUpdate TIMESTAMP WITHOUT TIME ZONe,
+  userUpdate VARCHAR,
+  PRIMARY KEY (consecVenta),
+  CONSTRAINT fkCliente
+  FOREIGN KEY (consecCli) REFERENCES tabCliente(consecCli),
+  CONSTRAINT fkAdministrador
+  FOREIGN KEY (idAdmin) REFERENCES tabAdministrador(idAdmin)
+  
+  
 );
 
-CREATE TABLE reg_borrados(
-  consec_reg BIGINT NOT NULL DEFAULT NEXTVAL('consec_reg'),
-  fec_delete TIMESTAMP WITHOUT TIME ZONe,
-  user_delete VARCHAR NOT NULL,
-  nom_tabla VARCHAR NOT NULL,
-  PRIMARY KEY (consec_reg)
+
+CREATE TABLE tabDetalleVenta(
+  consecDetVenta BIGINT NOT NULL DEFAULT NEXTVAL('consecDetVenta'),
+  consecVenta BIGINT NOT NULL,
+  eanArt VARCHAR NOT NULL,
+  nomArt VARCHAR NOT NULL,
+  cantArt INTEGER NOT NULL,
+  valUnit NUMERIC(10) NOT NULL,
+  subTotal NUMERIC(10) NOT NULL,
+  totalPagar NUMERIC(10) NOT NULL,
+  estado TEXT NOT NULL DEFAULT 'ACTIVO',
+  fecInsert TIMESTAMP WITHOUT TIME ZONE,
+  userInsert VARCHAR,
+  fecUpdate TIMESTAMP WITHOUT TIME ZONe,
+  userUpdate VARCHAR,
+  PRIMARY KEY (consecDetVenta),
+  CONSTRAINT fkVenta
+  FOREIGN KEY (consecVenta) REFERENCES tabEncabezadoVenta(consecVenta),
+  CONSTRAINT fkArticulo
+  FOREIGN KEY (eanArt) REFERENCES tabArticulo(eanArt)
+
+);
+
+
+CREATE TABLE tabRegBorrados(
+  consecReg BIGINT NOT NULL DEFAULT NEXTVAL('consecReg'),
+  fecDelete TIMESTAMP WITHOUT TIME ZONE,
+  userDelete VARCHAR NOT NULL,
+  nomTabla VARCHAR NOT NULL,
+  PRIMARY KEY (consecReg)
 );
