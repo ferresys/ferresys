b644324d262e92b691d41175460af37282503f51
davidadarme <timdaviarm@pm.me>
2023-09-20 20:55:40 -0500

    Backup para la base de datos

--- db/Postgres/Procedures/CopiaDeSeguridad.sql
+++ db/Postgres/Procedures/CopiaDeSeguridad.sql
@@ -1,5 +1,5 @@
 --@yocser revisar
--- crear trigger por errores de salida-entrada de datos
+-- crear trigger por errores de salida-entrada de datos en el front-end
 CREATE PROCEDURE backupDatabase
     @ferresysDB NVARCHAR(255),
     @RutaCopiaSeguridad NVARCHAR(255)
