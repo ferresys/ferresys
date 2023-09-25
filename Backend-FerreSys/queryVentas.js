8a15c7fad1324a521cc3b9a5acce4a5571d2a55e
davidadarme <timdavidarm@pm.me>
2023-09-25 01:36:11 -0500

    Nuevos Queries junto con el index actualizado para las tablas y autenticación de usuario

--- Backend-FerreSys/queryVentas.js
+++ Backend-FerreSys/queryVentas.js
@@ -1,7 +1,3 @@
-/*Comentarios
-Concatenar las tablas y crear un condicional/Query para por el tipo de consulta
-*/
-
 import dbPool from './conectionDB';
 
 export const consultarVentas = async () => {
@@ -9,38 +5,34 @@ export const consultarVentas = async () => {
     const ventas = await dbPool.connect();
     const result = await ventas.query(`
     SELECT
-        ev.idEncVenta,
-        ev.consecFactura,
-        ev.consecCotizacion,
-        ev.tipoFactura,
-        ev.estadoFactura,
-        ev.idCli,
-        tc.codCli,
-        tc.tipoCli,
-        CONCAT(tc.nomCli, ' ', tc.apeCli) AS nombreCompleto,
-        tc.telCli,
-        tc.emailCli,
-        tc.dirCli,
-        tc.nomRepLegal,
-        tc.nomEmpresa,
-        tdv.consecDetVenta,
-        tdv.eanArt,
-        tdv.cantArt,
-        tdv.valUnit,
-        tdv.subTotal,
-        tdv.iva,
-        tdv.descuento,
-        tdv.totalPagar
+        idEncVenta,
+        consecFactura,
+        consecCotizacion,
+        tipoFactura,
+        estadoFactura,
+        idCli,
+        codCli,
+        tipoCli,
+        CONCAT  nomCli, ' ',  apeCli) AS nombreCompleto,
+        telCli,
+        emailCli,
+        dirCli,
+        nomRepLegal,
+        nomEmpresa,
+        consecDetVenta,
+        eanArt,
+        cantArt,
+        valUnit,
+        subTotal,
+        iva,
+        descuento,
+        totalPagar
     FROM
         tabEncabezadoVenta
     INNER JOIN
-        tabCliente AS tc
-    ON
-        ev.idCli = tc.idCli
-    LEFT JOIN
-        tabDetalleVenta AS tdv
-    ON
-        ev.idEncVenta = tdv.idEncVenta;`);
+        tabDetalleVenta`);
+ 
+    // Concatenar las tablas y crear un condicional/Query para por el tipo de consulta
 
     ventas.release();
     return result.rows;
