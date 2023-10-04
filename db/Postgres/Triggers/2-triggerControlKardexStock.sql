a7f0c56fa3e78520d2738010f597b718c5132fc5
David Adarme <timdavidarm@pm.me>
2023-10-03 21:26:00 -0500

    Update trigger stock

--- db/Postgres/Triggers/2-triggerControlKardexStock.sql
+++ db/Postgres/Triggers/2-triggerControlKardexStock.sql
@@ -21,37 +21,37 @@ DECLARE
 
 BEGIN
 
-    --SELECT tipoMov INTO zTipoMov FROM tabKardex WHERE eanArt = NEW.enArt;
     SELECT valStock, stockMin, stockMax INTO zValStock, zStockMin, zStockMax FROM tabArticulo WHERE eanArt = NEW.eanArt;
     SELECT cantArt INTO zCantArt FROM tabDetalleVenta WHERE eanArt = NEW.eanArt;
 
-    -- SELECT ztipoMov WHERE ztipoMov = TRUE
-    -- ENTRADA / tipoMov = TRUE
+    IF FOUND THEN
+        RAISE NOTICE '%', zValStock;
 
-    IF zCantArt <= zStockMax AND zCantArt > 0 THEN
-        RAISE NOTICE 'Exitoso';
-    END IF;
+        -- ENTRADA / tipoMov = TRUE
+        IF zCantArt <= zStockMax AND zCantArt > 0 THEN
+            RAISE NOTICE 'Exitoso';
+        END IF;
+    
+        -- SALIDA / tipoMov = FALSE
+        IF zCantArt <= 0 THEN
+            RAISE NOTICE 'Debe ingresar una cantidad';
+        END IF;
 
-    -- SALIDA / tipoMov = FALSE
+        IF zCantArt > zValStock OR zCantArt > zStockMax THEN
+            RAISE NOTICE 'La cantidad de salida supera el stock disponible';
+        END IF;
 
-    IF zCantArt <= 0 THEN
-        RAISE NOTICE 'Debe ingresar una cantidad';
-    END IF;
+        IF ABS(zCantArt - zValStock) < zStockMin THEN
+            RAISE NOTICE 'No se puede realizar la operacion, el stock hara que sea menor que el stock minimo';
+        END IF;
 
-    IF zCantArt > zValStock OR zCantArt > zStockMax THEN
-        RAISE NOTICE 'La cantidad de salida supera el stock disponible';
-    END IF;
+        IF zCantArt = zValStock AND zValStock <= zStockMin THEN
+            RAISE NOTICE 'la cantidad supera las existencias minimas en stock';
+        END IF;
 
-    IF ABS(zCantArt - zValStock) < zStockMin THEN
-        RAISE NOTICE 'No se puede realizar la operacion, el stock hara que sea menor que el stock minimo';
-    END IF;
-
-    IF zCantArt = zValStock AND zValStock <= zStockMin THEN
-		RAISE NOTICE 'la cantidad supera las existencias minimas en stock';
-	END IF;
-
-    IF zValStock IS NULL OR zValStock <= 0 THEN
-        RAISE EXCEPTION 'No se puede realizar la operacion, stock negativo o en cero';
+        IF zValStock IS NULL OR zValStock <= 0 THEN
+            RAISE EXCEPTION 'No se puede realizar la operacion, stock negativo o en cero';
+        END IF;
     END IF;
 
     --SELECT * from tabArticulo
@@ -59,10 +59,10 @@ BEGIN
     --SELECT insertReciboMercancia('00000002', 30, 1000, '0-12', '2', '2', 'Alambre eléctrico');
     --SELECT insertReciboMercancia('00000002', 10, 8000, '0-12', '2', '2', 'Alambre eléctrico');
     --SELECT insertReciboMercancia('00000002', -700, 8000, '0-12', '2', '2', 'Alambre eléctrico');
-	--SELECT insertReciboMercancia('00000002', 700, 8000, '0-12', '2', '2', 'Alambre eléctrico');
-	--SELECT insertDetalleVenta ('00000002', 1000, 0);
+	--SELECT insertReciboMercancia('00000002', 350, 8000, '0-12', '2', '2', 'Alambre eléctrico');
+	--SELECT insertDetalleVenta ('00000002', 50, 0);
 
-    UPDATE tabArticulo SET valStock = zValStock WHERE eanArt = NEW.eanArt;
+    --UPDATE tabArticulo SET valStock = zValStock WHERE eanArt = NEW.eanArt;
 
   RETURN NEW;
 END;
@@ -73,15 +73,10 @@ LANGUAGE plpgsql;
 CREATE TRIGGER ControlKardexReciboMercancia
 BEFORE INSERT ON tabReciboMercancia
 FOR EACH ROW
-EXECUTE FUNCTION ControlKardex();
+EXECUTE FUNCTION controlKardex();
 
 -- Trigger para tabDetalleVenta
 CREATE TRIGGER ControlKardexDetalleVenta
 BEFORE INSERT ON tabDetalleVenta
 FOR EACH ROW
-EXECUTE FUNCTION ControlKardex();
-
-CREATE TRIGGER ControlKardexDetalleVenta
-AFTER INSERT ON tabArticulo
-FOR EACH ROW
-EXECUTE FUNCTION ControlKardex();
+EXECUTE FUNCTION controlKardex();
