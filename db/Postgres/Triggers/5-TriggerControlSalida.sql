1e404185ca86a119f5df364262c09429512c1753
davidadarme <timdavidarm@pm.me>
2023-10-05 19:32:48 -0500

    Actualización de backup

--- db/Postgres/Triggers/5-TriggerControlSalida.sql
+++ db/Postgres/Triggers/5-TriggerControlSalida.sql
@@ -8,7 +8,7 @@ DECLARE
     zStockMax tabArticulo.stockMax%type;
 
 BEGIN
-    -- obtenemos el valor del stock máximo de la tabArticulo
+    
     SELECT valStock, stockMin, stockMax INTO zValStock, zStockMin, zStockMax FROM tabArticulo WHERE eanArt = NEW.eanArt;
 
         --SELECT insertDetalleVenta ('00000001', 6, 0);
@@ -16,18 +16,18 @@ BEGIN
         --val min= 10
         CASE
             -- Verificar que el valor ingresado corresponda a una cantidad contable positiva
-            WHEN CAST(NEW.eanArt AS NUMERIC) <= 0 THEN
+            WHEN NEW.cantArt <= 0 THEN
                 RAISE EXCEPTION 'Debe ingresar una cantidad';
 
-            WHEN (CAST(NEW.eanArt AS numeric) + zValStock) <= zStockMin THEN
+            WHEN (NEW.cantArt + zValStock) <= zStockMin THEN
                 RAISE EXCEPTION 'Error stock min';
 
             -- Verifica que el valor ingresado no supere al atributo StockMax (Stock Máximo)
-            WHEN CAST(NEW.eanArt AS numeric) > zStockMax THEN
+            WHEN NEW.cantArt > zStockMax THEN
                 RAISE EXCEPTION 'La cantidad de salida supera el stock máximo';
 
             -- No puede sacar la misma cantidad de stock actual ya que viola la restricción del stock mínimo
-            WHEN CAST(NEW.eanArt AS numeric) = zValStock THEN
+            WHEN NEW.cantArt = zValStock THEN
                 RAISE EXCEPTION 'La cantidad supera las existencias mínimas en stock';
             
             -- Verificar el valor de stock no esté en mínimo o sea menor/igual que cero
