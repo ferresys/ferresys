a03d2bd16c0f9ad0dabd812b87eb08d1f2eb5c11
jacob <yjchavez7@soy.sena.edu.co>
2023-11-01 00:48:37 -0500

    creacion de modulos de consultas y registros

---
+++ front/js/formularios/cancelar.js
@@ -0,0 +1,6 @@
+
+document.getElementById("cancelButton").addEventListener("click", function() {
+    // Aquí puedes agregar cualquier lógica adicional que desees antes de recargar la página
+    location.reload(); // Esto recargará la página
+});
+
---
+++ front/js/formularios/clientes.js
@@ -0,0 +1,36 @@
+/* formulario clientes*/
+
+document.addEventListener("DOMContentLoaded", function () {
+  const form = document.getElementById("registroForm");
+
+  form.addEventListener("submit", function (e) {
+    e.preventDefault();
+    const formData = new FormData(form);
+
+    // Crear un objeto para almacenar los datos
+    const data = {};
+
+    formData.forEach((value, key) => {
+      data[key] = value;
+    });
+
+    fetch("http://localhost:4000/clientes", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        // Manejar la respuesta del servidor (si es necesario)
+        console.log(data);
+
+        // Limpiar los campos del formulario
+        form.reset();
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  });
+});
