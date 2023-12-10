615709e98681aa8944f4353f896c4f0a14f30e5f
davidadarme <timdavidarm@pm.me>
2023-12-10 15:12:19 -0500

    Update API endpoint and add authorization
    This commit updates the API endpoint for cliente, proveedor, categorias, marcas retrieval by adding the authorization header using the token stored in the local storage. This improves the security and ensures that only authorized users can access the clients' data.

--- public/js/clientes.js
+++ public/js/clientes.js
@@ -2,33 +2,36 @@ document.getElementById('consultarButton').addEventListener('click', () => {
     const token = localStorage.getItem('token');
 
     fetch('http://localhost:4000/clientes', {
-        credentials: 'include',
+        method: 'GET',
         headers: {
-            'Authorization': `Bearer ${token}`
+            'Authorization': `Bearer ${token}`,
         }
     })
-    .then(response => response.json())
-    .then(data => {
-        const tableBody = document.querySelector('#clienteTable tbody');
-        tableBody.innerHTML = '';
+        .then(response => response.json())
+        .then(data => {
+            const tableBody = document.querySelector('#clienteTable tbody');
+            tableBody.innerHTML = ''; // Limpiar la tabla antes de agregar nuevos datos
 
-        data.forEach(cliente => {
-            const row = document.createElement('tr');
-            row.innerHTML = `
-                <td>${cliente.idcli}</td>
-                <td>${cliente.tipocli}</td>
-                <td>${cliente.nomcli}</td>
-                <td>${cliente.apecli}</td>
-                <td>${cliente.nomreplegal}</td>
-                <td>${cliente.nomempresa}</td>
-                <td>${cliente.telcli}</td>
-                <td>${cliente.emailcli}</td>
-                <td>${cliente.dircli}</td>
-            `;
-            tableBody.appendChild(row);
+            data.forEach(cliente => {
+                const row = document.createElement('tr');
+                row.innerHTML = `
+                    
+                    <td>${cliente.idcli}</td>
+                    <td>${cliente.tipocli}</td>
+                    <td>${cliente.nomcli}</td>
+                    <td>${cliente.apecli}</td>
+                    <td>${cliente.nomreplegal}</td>
+                    <td>${cliente.nomempresa}</td>
+                    <td>${cliente.telcli}</td>
+                    <td>${cliente.emailcli}</td>
+                    <td>${cliente.dircli}</td>
+                    
+                `;
+                tableBody.appendChild(row);
+            });
+
+        })
+        .catch(error => {
+            console.error('Error al realizar la consulta:', error);
         });
-    })
-    .catch(error => {
-        console.error('Error al realizar la consulta:', error);
-    });
 });
