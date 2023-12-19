e5c60e29fee94fec53e364bb85cb6a98bcde254e
davidadarme <timdavidarm@pm.me>
2023-12-19 02:42:05 -0500

    testing

--- app/config/config-database.js
+++ app/config/config-database.js
@@ -4,11 +4,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const pool = new Pool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
+  connectionString: process.env.DATABASE_URL
 });
 
 export default pool;
